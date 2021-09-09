4647d5ef0eab1b4066b7fa5778d649644ed1d55b
Lucas Yunkyu Lee <me@luc.li>
2021-09-10 01:28:55 +0900

    feat(build): implement rendering to html

--- src/build/config.ts
+++ src/build/config.ts
@@ -4,3 +4,5 @@ export const buildDir = ".soar"
 export const outDir = join(buildDir, "out")
 export const rootDir = "."
 export const pagesDir = join(rootDir, "pages")
+
+export const distDir = "dist"
--- src/build/index.ts
+++ src/build/index.ts
@@ -1,10 +1,12 @@
 import { generateBuildDir } from "./manage-build-dir.js"
 import transpile from "./transpile.js"
 import glob from "fast-glob"
-import { outDir } from "./config.js"
-import { join, extname } from "node:path"
+import { distDir, outDir } from "./config.js"
+import { join, extname, dirname } from "node:path"
 
 import { replaceExt } from "./utils.js"
+import { cwd } from "node:process"
+import { mkdir, writeFile } from "node:fs/promises"
 
 const build = async (): Promise<void> => {
   await generateBuildDir()
@@ -18,11 +20,33 @@ const build = async (): Promise<void> => {
     }
   )
 
-  Promise.all(
+  const transpiled = await Promise.all(
+    // transpiling loop
     files
       .filter((file) => matchExts.includes(extname(file)))
-      .map((file) => transpile(file, join(outDir, replaceExt(file, ".js"))))
+      .map((file) => {
+        const path = join(outDir, replaceExt(file, ".js"))
+        transpile(file, path)
+        return path
+      })
   )
+
+  const built = (
+    await Promise.all(
+      // rendering loop
+      transpiled.map(async (file) => {
+        const pageFn = await import(join(cwd(), file))
+        if (typeof pageFn.default === "function") {
+          const path = join(distDir, replaceExt(file, ".html"))
+          await mkdir(dirname(path), { recursive: true })
+          await writeFile(path, pageFn.default())
+          return path
+        }
+      })
+    )
+  ).filter((file): file is string => !!file)
+
+  console.log(`Built ${built.length} files.`)
 }
 
 export default build
