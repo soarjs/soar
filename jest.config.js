6ddc93795087e1a88b43109a240eaeda9dbe5932
Lucas Yunkyu Lee <me@luc.li>
2021-09-10 17:15:35 +0900

    test: update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
-export default {
+/** @type {import("@jest/types").Config.InitialOptions} */
+const config = {
   preset: "ts-jest",
   resolver: "<rootDir>/jest-resolver.cjs",
+  modulePathIgnorePatterns: ["lib/"],
 }
+
+export default config
