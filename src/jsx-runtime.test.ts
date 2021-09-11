9d8433cee04a213320c1173fe9bc0a0c1d593fa9
Lucas Yunkyu Lee <me@luc.li>
2021-09-11 21:44:11 +0900

    test(jsx-runtime): fix test to match jsx transform

--- src/jsx-runtime.test.ts
+++ src/jsx-runtime.test.ts
@@ -1,5 +1,5 @@
 import { jsx } from "./jsx-runtime"
 
 test("jsx rendering", () => {
-  expect(jsx("tag", "int")).toEqual("<tag>int</tag>")
+  expect(jsx("tag", { children: "int" })).toEqual("<tag>int</tag>")
 })
