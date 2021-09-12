bce0b8678e00156481bea9a095ad97cb7407775a
Lucas Yunkyu Lee <me@luc.li>
2021-09-12 15:32:00 +0900

    feat: support html properties

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,21 +1,21 @@
 type DefaultProps = {
   children?: string | string[]
+  [property: string]: any
 }
 
-const jsx = (t: string, o: string | DefaultProps): string => {
-  if (typeof o === "string") {
-    return `<${t}>${o}</${t}>`
-  }
+const renderProps = (props: DefaultProps) =>
+  Object.entries(props)
+    .filter((prop) => prop[0] !== "children")
+    .map((prop) => ` ${prop[0]}="${prop[1]}"`)
+    .join("")
 
+const jsx = (t: string, o: DefaultProps): string => {
   if (typeof o.children === "string") {
-    return `<${t}>${o.children}</${t}>`
-  }
-
-  if (Array.isArray(o.children)) {
-    return `<${t}>${o.children.join("")}</${t}>`
-  }
-
-  return `<${t}>${o}</${t}>`
+    return `<${t}${renderProps(o)}>${o.children}</${t}>`
+  } else if (Array.isArray(o.children)) {
+    return `<${t}${renderProps(o)}>${o.children.join("\n")}</${t}>`
+  } else return `<${t}${renderProps(o)}></${t}>`
 }
 
+export * as JSX from "./jsx.js"
 export { jsx }
