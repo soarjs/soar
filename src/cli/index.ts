d484198c7329333be35f178a65b143a1a1791704
Lucas Yunkyu Lee <me@luc.li>
2021-09-25 12:54:24 +0900

    feat: implement basic project initialization

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,6 +2,7 @@
 
 import { Command } from "commander"
 import build from "../build/index.js"
+import init from "../init/index.js"
 import { createRequire } from "module"
 
 const packageJson = createRequire(import.meta.url)("../../package.json")
@@ -14,6 +15,14 @@ program
   .description("Build static pages from JSX")
   .action(async () => {
     await build()
+  })
+
+program
+  .command("init")
+  .argument("<directory>", "directory to initialize in")
+  .description("Initialize a new Soar project")
+  .action(async (directory) => {
+    await init(directory)
   })
 
 program.parse(process.argv)
---
+++ src/init/index.ts
@@ -0,0 +1,52 @@
+import { mkdir, readdir, writeFile, lstat, copyFile } from "node:fs/promises"
+import { spawn } from "node:child_process"
+import { basename, dirname, join, resolve } from "node:path"
+import { fileURLToPath } from "node:url"
+
+const init = async (dir: string): Promise<void> => {
+  await install(dir)
+}
+
+const cp = async (src: string, dest: string) => {
+  mkdir(dest, { recursive: true })
+  for (const file of await readdir(src)) {
+    if ((await lstat(join(src, file))).isFile()) {
+      copyFile(join(src, file), join(dest, file))
+    } else {
+      await cp(join(src, file), join(dest, file))
+    }
+  }
+}
+
+const install = async (dir: string) => {
+  console.log(`Beginning Soar initialization into ${dir}`)
+
+  const packageJson = {
+    name: basename(dir),
+    version: "0.1.0",
+    scripts: {
+      build: "soar build",
+    },
+  }
+
+  const command = "npm"
+
+  await mkdir(dir, { recursive: true })
+  await writeFile(
+    join(dir, "package.json"),
+    JSON.stringify(packageJson, null, 2) + "\n"
+  )
+
+  const templateDir = resolve(fileURLToPath(import.meta.url), "../template")
+  console.log(templateDir)
+
+  // await cp(templateDir, dir)
+
+  const ls = spawn(command, ["install", "--save", dependencies.join(" ")], {
+    cwd: dir,
+  })
+}
+
+const dependencies = ["soar"]
+
+export default init
